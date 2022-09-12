ae44fcb5f3bd3e9958a1ba40f24d50a9ed479e3a
Volkov Andrey <volkov_andrey@clout.one>
2022-09-12 18:44:19 +0500

    fix links, and add placeholder text in home page

--- code/src/pages/Home/Home.tsx
+++ code/src/pages/Home/Home.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Search } from '../../components/Search/Search'
 import s from './Home.module.css'
 
 export const Home = () => {
+  const [currentPlaceholder, setCurrentPlaceholder] = useState(null)
+
+  const placeholders = [
+    {title:'axios'},
+    {title:'Yandex'},
+    {title:'Node.js'},
+    {title:'public-apis'},
+    {title:'facebook'},
+  ]
+  
+  const getRandomNum = (num:number) => Math.floor(Math.random() * num)
+  // useEffect(()=>{
+  // 
+  // },[])
   return (
     <div className={s.home}>
-      <h1 className={s.title}>Поиск репозиториев ещё никогда не был таким простым</h1>
+      <div className={s.wrapper}>
+        <h1 className={s.title}>Поиск репозиториев ещё никогда не был таким простым</h1>
+        {/* {currentPlaceholder !== null && <p>Например, введите <span className={s.placeholder}>{currentPlaceholder}</span></p>} */}
+        <p>Например, введите <span className={s.placeholder}>{placeholders[getRandomNum(placeholders.length)].title}</span></p>
+      </div>
       <Search/>
     </div>
   )
